b5c0d46214986b5a08aec54cabc5bd85f1fd4859
s-tyda <Sebastian.Tyda@Live.umcs.edu.pl>
2021-03-11 18:00:52 +0100

    Added more solutions to exercise 11

--- #06_petle/#06-zadania/11.py
+++ #06_petle/#06-zadania/11.py
@@ -1,11 +1,38 @@
 # Napisz program, który stwierdza czy liczba jest pierwsza.
+import sympy
 
+# Wersja z dodawaniem
 suma = 0
 a = int(input())
 for i in range(2, a):
     if a % i != 0:
         suma += 1
-if suma == a - 2:
-    print("Prawda")
-else:
-    print("Fałsz")
+print(suma == a - 2)
+
+# Wersja z mnożeniem
+wynik = 1
+for i in range(2, a):
+    if a % i == 0:
+        wynik *= 0
+    else:
+        wynik *= 1
+print(bool(wynik))
+
+# Wersja z flagą
+wynik = 1
+for i in range(2, a):
+    if a % i == 0:
+        wynik = 0
+print(bool(wynik))
+
+# Wersja z flagą i łamaniem pętli
+wynik = 1
+for i in range(2, a):
+    if a % i == 0:
+        wynik = 0
+        break
+print(bool(wynik))
+
+print(sympy.isprime(a))
+
+print(all([a % i != 0 for i in range(2, a)]))
